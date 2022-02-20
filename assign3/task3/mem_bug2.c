3723ba4549c678eb21f952fbac76bd58f6d57b67
salvatore-ardisi <salvatore.ardisi@gmail.com>
2022-02-20 18:29:34 -0500

    completed task3

--- assign3/task3/mem_bug2.c
+++ assign3/task3/mem_bug2.c
@@ -6,13 +6,13 @@ int main(int argc, char *argv[])
 
   int i, *a;
 
-  a = malloc(sizeof(int) * 10);
+  a = malloc(sizeof(int) * 11);
 
-  for (i = 0; i < 10; i++)
+  for (i = 0; i <= 10; i++)
   {
     a[i] = i;
   }
-  for (i = 0; i < 10; i++)
+  for (i = 0; i <= 10; i++)
   {
     printf("%d\n", a[i]);
   }
