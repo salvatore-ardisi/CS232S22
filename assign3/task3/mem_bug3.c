93b3b01cd3d0827cb51a875668a1549c26969bab
salvatore-ardisi <salvatore.ardisi@gmail.com>
2022-02-20 14:06:41 -0500

    completed mem_bug3

--- assign3/task3/mem_bug3.c
+++ assign3/task3/mem_bug3.c
@@ -1,32 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
-int * makearray(int size,int base){
+int *makearray(int size, int base)
+{
+  // dynamically alloc mem
+  int *array = malloc((size) * sizeof(int));
 
-  int array[size];
+  // int array[size];
   int j;
 
-  for(j=0;j<size;j++)
-    array[j] = base*=2; //doubling base
+  for (j = 0; j < size; j++)
+    array[j] = base *= 2; // doubling base
 
   return array;
 }
 
-int main(){
-  int * a1 = makearray(5,2);
-  int * a2 = makearray(10,3);
-  int j, sum=0;
+int main()
+{
+  int *a1 = makearray(5, 2);
+  int *a2 = makearray(10, 3);
+  int j, sum = 0;
 
-  for(j=0;j<5;j++){
-    printf("%d ",a1[j]);
-    sum+=a1[j];
+  for (j = 0; j < 5; j++)
+  {
+    printf("%d ", a1[j]);
+    sum += a1[j];
   }
   printf("\n");
 
-  for(j=0;j<10;j++){
-    printf("%d ",a2[j]);
-    sum+=a2[j];
+  for (j = 0; j < 10; j++)
+  {
+    printf("%d ", a2[j]);
+    sum += a2[j];
   }
   printf("\n");
 
   printf("SUM: %d\n", sum);
+  // free first makearray
+  free(a1);
+  // free second array
+  free(a2);
 }
