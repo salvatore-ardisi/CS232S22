648d7ec228ff70b105c7050aa64a94e3519eb5a3
salvatore-ardisi <salvatore.ardisi@gmail.com>
2022-02-20 13:40:27 -0500

    completed mem_bug1

--- assign3/task3/mem_bug1.c
+++ assign3/task3/mem_bug1.c
@@ -2,18 +2,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char * argv[]){
+int main(int argc, char *argv[])
+{
 
-  int * a = malloc(sizeof(int *));
+  int *a = malloc(sizeof(int *));
 
-  *a = 10;  
+  *a = 10;
 
   printf("%d\n", *a);
 
-  a = malloc(sizeof(int *)*3);
+  free(a);
+
+  a = malloc(sizeof(int *) * 3);
   a[0] = 10;
   a[1] = 20;
   a[2] = 30;
 
   printf("%d %d %d\n", a[0], a[1], a[2]);
+  free(a);
 }
