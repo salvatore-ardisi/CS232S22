16fbbbfb23c0beac8a8594723e7e1bd9bceb17c2
salvatore-ardisi <salvatore.ardisi@gmail.com>
2022-02-20 13:49:03 -0500

    completed mem_bug2

--- assign3/task3/mem_bug2.c
+++ assign3/task3/mem_bug2.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char * argv[]){
+int main(int argc, char *argv[])
+{
 
   int i, *a;
 
-  a = malloc(sizeof(int)*10);
+  a = malloc(sizeof(int) * 10);
 
-  for(i=0;i <= 10; i++){
+  for (i = 0; i < 10; i++)
+  {
     a[i] = i;
   }
-  for(i=0;i <= 10; i++){
+  for (i = 0; i < 10; i++)
+  {
     printf("%d\n", a[i]);
   }
-
+  free(a);
 }
