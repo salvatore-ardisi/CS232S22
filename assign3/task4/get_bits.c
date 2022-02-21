2941eca74269e7f59c52d46f0c77eb30ff902396
salvatore-ardisi <salvatore.ardisi@gmail.com>
2022-02-21 18:21:00 -0500

    get_bits attempt

--- assign3/task4/get_bits.c
+++ assign3/task4/get_bits.c
@@ -1,20 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
-// Note, the bits are counted from right to left. 
+// Note, the bits are counted from right to left.
 // Return the bit states of x within range of [start, end], in which both are inclusive.
 // Assume 0 <= start & end <= 31
-unsigned * get_bits(unsigned x,
-                 unsigned start,
-                 unsigned end) {
-    
-   return NULL;
-    // YOUR CODE HERE
-    // Returning NULL is a placeholder
-    // get_bits dynamically allocates an array a and set a[i] = 1 when (i+start)-th bit
-    // of x is 1, otherwise set a[i] = 0;
-    // At last, get_bits returns the address of the array.
+unsigned *get_bits(unsigned x, unsigned start, unsigned end)
+{
+    // get_bits dynamically allocates an array a
+    unsigned int *a = malloc((end - start) * sizeof(unsigned int));
+
+    for (unsigned int i = start; i <= end; i++)
+    {
+        // set a[i] = 1 when (i+start)-th bit of x is 1
+        if (x >= start && x < (x ^ end))
+        {
+            // set a[i] = 1
+            a[i] = 1;
+        }
+        // otherwise
+        else
+        {
+            // set a[i] = 0
+            a[i] = 0;
+        }
+    }
+    // returns the address of the array
+    return a;
 }
-
-
-
