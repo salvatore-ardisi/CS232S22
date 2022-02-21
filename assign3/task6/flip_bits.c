1df115ec2c508657120bdf5a42a21507467d7f8c
salvatore-ardisi <salvatore.ardisi@gmail.com>
2022-02-21 18:26:55 -0500

    flip_bits attempt

--- assign3/task6/flip_bits.c
+++ assign3/task6/flip_bits.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Note, the bits are counted from right to left. 
+// Note, the bits are counted from right to left.
 // Flip the bits of x within range [start, end], in which both are inclusive.
 // Assume 0 <= start & end <= 31
-void flip_bits(unsigned * x,
-              unsigned start,
-              unsigned end) {
-    // YOUR CODE HERE
+void flip_bits(unsigned *x,
+               unsigned start,
+               unsigned end)
+{
+    unsigned *a = (unsigned *)malloc(sizeof(unsigned) * (end - start + 1));
+
+    for (unsigned i = 0; i < (end - start) + 1; i++)
+    {
+        a[i] = ~*x;
+    }
 }
