801173b1d2a635373d766f0cd7e534ed82d3a32f
salvatore-ardisi <salvatore.ardisi@gmail.com>
2022-02-20 22:34:47 -0500

    attempt convert_2

--- assign3/task2/convert_2.c
+++ assign3/task2/convert_2.c
@@ -1,19 +1,60 @@
-#include<stdio.h>
-#include<stdlib.h>
-//Program to convert a 32-bit Decimal numbers to Binary
-//1) Do not change the signature of convert_2, or your task receives zero
-//2) No globals allowed.
-char* convert_2(int dec)
+#include <stdio.h>
+#include <stdlib.h>
+// Program to convert a 32-bit Decimal numbers to Binary
+// 1) Do not change the signature of convert_2, or your task receives zero
+// 2) No globals allowed.
+char *convert_2(int dec)
 {
-	//TODO: your implementation
+	// TODO: your implementation
+	char *bin = malloc(33 * sizeof(char));
+
+	// if dec is negative
+	if (dec < 0)
+	{
+		dec *= -1;
+		for (int i = 0; i < 32; i++)
+		{
+			if (dec % 2 == 1)
+			{
+				bin[33 - i] = '1';
+			}
+			else
+			{
+				bin[33 - i] = '0';
+			}
+			dec /= 2;
+		}
+	}
+	// otherwise
+	else
+	{
+		for (int i = 0; i < 32; i++)
+		{
+			if (dec % 2 == 1)
+			{
+				bin[33 - i] = '1';
+			}
+			else
+			{
+				bin[33 - i] = '0';
+			}
+			dec /= 2;
+		}
+	}
+
+	bin[33] = '\0';
+
+	return bin;
 }
 
-int main() {
+int main()
+{
 	int n;
-	char * bin;
+	char *bin;
 	printf("Enter the Decimal Number\n");
-	scanf("%d",&n);
+	scanf("%d", &n);
 	bin = convert_2(n);
-	printf("The Binary Notation of %d is\t %s\n", n, bin);
-	//TODO: do we need to release the memory of bin?
+	printf("The Binary Notation of %d is\t 0b%s\n", n, bin);
+	// TODO: do we need to release the memory of bin?
+	free(bin);
 }
