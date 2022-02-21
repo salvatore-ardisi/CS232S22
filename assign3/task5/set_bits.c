53d99efec402580faf5a5d3baf37176671145fc5
salvatore-ardisi <salvatore.ardisi@gmail.com>
2022-02-21 18:20:07 -0500

    set_bits attempt

--- assign3/task5/set_bits.c
+++ assign3/task5/set_bits.c
@@ -3,14 +3,27 @@
 
 // Set the bits of x within range of [start, end], in which both are inclusive
 // Assume 0 <= start & end <= 31
-void set_bits(unsigned * x,
-             unsigned start,
-             unsigned end,
-             unsigned *v) {
+void set_bits(unsigned *x,
+              unsigned start,
+              unsigned end,
+              unsigned *v)
+{
     // YOUR CODE HERE
     // No return value
     // v points to an array of at least (end-start+1) unsigned integers.
-    // if v[i] == 0, then set (i+start)-th bit of x zero, otherwise, set (i+start)-th bit of x one.
+    // if v[i] == 0, then set (i+start)-th bit of x zero, otherwise, set (i+start)-th bit of x one
+    for (int i = start; i <= end; i++)
+    {
+        if (v[i] == 0)
+        {
+            // set ( i + start)-th bit of x zero
+            v[i] &= *x;
+        }
+        // otherwise
+        else
+        {
+            // set(i + start) - th bit of x one
+            v[i] |= *x;
+        }
+    }
 }
-
-
