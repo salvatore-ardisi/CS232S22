e1274dd5b423652ccc980318e3aae3d67733ada3
salvatore-ardisi <salvatore.ardisi@gmail.com>
2022-02-21 23:41:25 -0500

    get_bits attempt

--- assign3/task4/get_bits.c
+++ assign3/task4/get_bits.c
@@ -6,24 +6,22 @@
 // Assume 0 <= start & end <= 31
 unsigned *get_bits(unsigned x, unsigned start, unsigned end)
 {
-    // get_bits dynamically allocates an array a
-    unsigned *a = (unsigned *)malloc(sizeof(unsigned) * (end - start + 1));
+	// get_bits dynamically allocates an array a
+	unsigned *a = (unsigned *)malloc(sizeof(unsigned) * (end - start + 1));
 
-    for (unsigned int i = start; i <= end; i++)
-    {
-        // set a[i] = 1 when (i+start)-th bit of x is 1
-        if (x >= start && x < (x ^ end))
-        {
-            // set a[i] = 1
-            a[i] = 1;
-        }
-        // otherwise
-        else
-        {
-            // set a[i] = 0
-            a[i] = 0;
-        }
-    }
-    // returns the address of the array
-    return a;
+	int temp = 1;
+
+	for (unsigned int i = 0; i <= end; i++)
+	{
+		if (i >= start && x < (x ^ temp))
+		{
+			a[i] = 0;
+		}
+		else
+		{
+			a[i] = 1;
+		}
+		temp <<= 1;
+	}
+	return a;
 }
