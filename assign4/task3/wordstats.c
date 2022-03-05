911548f14b7ef8c356218f6b0ce85f918620ee72
salvatore-ardisi <salvatore.ardisi@gmail.com>
2022-03-05 16:54:41 -0500

    attempted wordstats.c

--- assign4/task3/wordstats.c
+++ assign4/task3/wordstats.c
@@ -2,19 +2,37 @@
 #include <string.h>
 #define MAX_BUF 1024
 
-int main () {
+int main()
+{
 
   /* Zero out the array */
   int letter_frequency[26] = {0};
   int len, i;
   char buf[MAX_BUF];
 
-  do {
+  len = strlen(buf);
+
+  do
+  {
+    fgets(buf, MAX_BUF, stdin);
+
+    for (int i = 0; i < len; ++i)
+    {
+      if (buf[i] >= 'A' && buf[i] <= 'Z')
+      {
+        letter_frequency[buf[i] - 32]++;
+      }
+      else if (buf[i] >= 'a' && buf[i] <= 'z')
+      {
+        letter_frequency[buf[i] - 32]++;
+      }
+    }
   } while (len > 1);
 
   printf("Distribution of letters in corpus:\n");
-  for (i = 0; i < 26; i++) {
+  for (i = 0; i < 26; i++)
+  {
     // 'A' is ASCII code 65
-    printf("%c: %d\n", 65+i, letter_frequency[i]);
+    printf("%c: %d\n", 65 + i, letter_frequency[i]);
   }
 }
