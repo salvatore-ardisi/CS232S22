921c31e4bdb8be1486f388e84bb2251ca7e673e4
salvatore-ardisi <salvatore.ardisi@gmail.com>
2022-03-04 21:18:00 -0500

    testing uppercase.c

--- assign4/task1/uppercase.c
+++ assign4/task1/uppercase.c
@@ -1,23 +1,38 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[]) {
-/* example:
- *  char str[10090];
-   int ch, n = 0;
+int main(int argc, char *argv[])
+{
+   /* example:
+    *  char str[10090];
+      int ch, n = 0;
 
-   while ((ch = getchar()) != EOF && n < 1000) {
-      str[n] = ch;
-      ++n;
+      while ((ch = getchar()) != EOF && n < 1000) {
+         str[n] = ch;
+         ++n;
+      }
+
+      for (int i = 0; i < n; ++i)
+         putchar(str[i]);
+
+
+      return 0;
+
+    */
+
+   char ch;
+   ch = getchar();
+   while (ch && ch != EOF)
+   {
+      if (ch >= 97 && ch <= 122)
+      {
+         putchar(ch - 32);
+      }
+      else
+      {
+         putchar(ch);
+      }
+      ch = getchar();
    }
 
-   for (int i = 0; i < n; ++i)
-      putchar(str[i]);
-
-
    return 0;
-
- */
-
-    return 0;
 }
-
