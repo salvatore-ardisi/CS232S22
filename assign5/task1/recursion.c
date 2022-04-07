e5d7a77aa3592aad68298872d2ec6a77b4185002
Salvatore Ardisi <salvatore.ardisi@gmail.com>
2022-04-07 19:28:15 -0400

    completed task1

--- assign5/task1/recursion.c
+++ assign5/task1/recursion.c
@@ -4,36 +4,34 @@
 
 /* both of these routines print and sum up numbers i where start <= i < stop */
 
-long
-printSumRangeIterative(int start, int stop)
+long printSumRangeIterative(int start, int stop)
 {
     int i;
     long sum = 0;
-    for(i = start; i < stop; i++) {
+    for (i = start; i < stop; i++)
+    {
         printf("%d\n", i);
-	sum += i;
+        sum += i;
     }
     return sum;
 }
 
-long
-printSumRangeRecursive(int start, int stop)
+long printSumRangeRecursive(int start, int stop)
 {
-    long sum=0;
-    if(start < stop) {
+    long sum = 0;
+    if (start < stop)
+    {
         printf("%d\n", start);
-        sum=start+printSumRangeRecursive(start+1, stop);
-
+        sum = start + printSumRangeRecursive(start + 1, stop);
     }
     return sum;
 }
 
-
-int
-main(int argc, char **argv)
+int main(int argc, char **argv)
 {
 
-    if(argc != 2) {
+    if (argc != 2)
+    {
         fprintf(stderr, "Usage: %s an-integer\n", argv[0]);
         return 1;
     }
