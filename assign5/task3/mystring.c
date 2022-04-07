e452be394cb0ec2b1dbbbb7068343b2fb1f73043
Salvatore Ardisi <salvatore.ardisi@gmail.com>
2022-04-07 19:29:17 -0400

    completed task3

--- assign5/task3/mystring.c
+++ assign5/task3/mystring.c
@@ -6,30 +6,34 @@
 #include "mystring.h"
 
 /* Define what our struct is */
-struct mystring_t {
+struct mystring_t
+{
 	size_t size;
 	char *data;
 };
 
 /* Utility function to handle allocation failures. In this
    case we print a message and exit. */
-static void allocation_failed() {
-    fprintf(stderr, "Out of memory.\n");
-    exit(1);
+static void allocation_failed()
+{
+	fprintf(stderr, "Out of memory.\n");
+	exit(1);
 }
 
 /* Bad example of how to create a new mystring */
-mystring_t *bad_string_new() {
+mystring_t *bad_string_new()
+{
 	/* Create the string and a pointer to it */
-        /*This is a bad constructor as v gets destroyed once contructor returns*/
+	/*This is a bad constructor as v gets destroyed once contructor returns*/
 	mystring_t *retval, v;
 	retval = &v;
 
 	/* Initialize attributes */
-	retval->size = 1; //why size+1? size is the actual char lenght. 
-			  //we must reseve an extra space for null
-	retval->data = (char*) malloc((retval->size+1)*sizeof(char));
-	if (retval->data == NULL) {
+	retval->size = 1; // why size+1? size is the actual char lenght.
+					  // we must reseve an extra space for null
+	retval->data = (char *)malloc((retval->size + 1) * sizeof(char));
+	if (retval->data == NULL)
+	{
 		allocation_failed();
 	}
 
@@ -39,15 +43,17 @@ mystring_t *bad_string_new() {
 }
 
 /* Another suboptimal way of creating a string */
-mystring_t also_bad_string_new() {
+mystring_t also_bad_string_new()
+{
 	/* Create the string */
 	/*why is this bad? does v get destroyed upon returning? */
 	mystring_t s;
 
 	/* Initialize attributes */
-	s.size = 1; //why size+1? an extra space for null
-	s.data = (char*) malloc((s.size+1)*sizeof(char));
-	if (s.data == NULL) {
+	s.size = 1; // why size+1? an extra space for null
+	s.data = (char *)malloc((s.size + 1) * sizeof(char));
+	if (s.data == NULL)
+	{
 		allocation_failed();
 	}
 	s.data[0] = ' ';
@@ -58,40 +64,91 @@ mystring_t also_bad_string_new() {
 /* Create a new string with a size (length) of 1
    and set its single component to zero... the
    RIGHT WAY */
-mystring_t *mystring_new() {
+mystring_t *mystring_new()
+{
 	mystring_t *retval;
-	//YOUR CODE HERE
+	// YOUR CODE HERE
+	retval = (mystring_t *)malloc(sizeof(mystring_t));
+	if (retval == NULL)
+	{
+		allocation_failed();
+	}
+	retval->size = 1;
+	retval->data = (char *)malloc(sizeof(retval->data));
+	if (retval->data == NULL)
+	{
+		free(retval);
+		allocation_failed();
+	}
+	retval->data[0] = ' ';
+	retval->data[retval->size] = '\0';
 	return retval;
 }
 
 /* Return the value at the specified location/component "loc" of the mystring */
-char mystring_get(mystring_t *s, size_t loc) {
-	//YOUR CODE HERE
-	return ' ';
+char mystring_get(mystring_t *s, size_t loc)
+{
+	// YOUR CODE HERE
+	if (loc < s->size && loc >= 0)
+	{
+		return s->data[loc];
+	}
+	else
+	{
+		perror("Error");
+		return 0;
+	}
 }
 
 /* Free up the memory allocated for the passed mystring.
    Remember, you need to free up ALL the memory that was allocated. */
-void mystring_delete(mystring_t *s) {
+void mystring_delete(mystring_t *s)
+{
 	/* YOUR CODE HERE */
+	free(s->data);
+	free(s);
 }
 
-int mystring_get_len(mystring_t *s) {
+int mystring_get_len(mystring_t *s)
+{
 	/* YOUR CODE HERE */
+	return s->size;
 }
 
-char* mystring_get_data(mystring_t *s) {
+char *mystring_get_data(mystring_t *s)
+{
 	/* YOUR CODE HERE*/
 	return s->data;
 }
-void mystring_cat(mystring_t *s, char *s2) {
+void mystring_cat(mystring_t *s, char *s2)
+{
 	/* YOUR CODE HERE*/
+	int i = s->size, j;
+	for (j = 0; s2[j] != '\0'; i++, j++)
+	{
+		s->data[i] = s2[j];
+	}
+	s->data[i] = '\0';
 }
 
 /* Set a value in the mystring. If the extra memory allocation fails, call
    allocation_failed(). Unset space should be value of space */
-void mystring_set(mystring_t *s, size_t loc, char value) {
+void mystring_set(mystring_t *s, size_t loc, char value)
+{
 	/* YOUR CODE HERE*/
-
+	if (loc < s->size && loc >= 0)
+	{
+		s->data[loc] = value;
+	}
+	else
+	{
+		s->data = (char *)realloc(s->data, sizeof(char) * (loc + 1));
+		for (int i = s->size; i < loc + 1; i++)
+		{
+			s->data[i] = 0;
+		}
+		s->data[loc] = value;
+		s->size = loc + 1;
+	}
 	return;
 }
